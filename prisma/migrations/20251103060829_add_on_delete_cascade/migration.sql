3ef6788ccd0c6228c2c2439c197f9630631e61aa
YooInHak <dlsgkr2856@gmail.com>
2025-11-03 15:09:11 +0900

    [ADD] onDelete 관계 설정(model삭제시 comment도 같이삭제)

---
+++ prisma/migrations/20251103060829_add_on_delete_cascade/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "public"."Comment" DROP CONSTRAINT "Comment_articleId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Comment" DROP CONSTRAINT "Comment_productId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_productId_fkey" FOREIGN KEY ("productId") REFERENCES "product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_articleId_fkey" FOREIGN KEY ("articleId") REFERENCES "Article"("id") ON DELETE CASCADE ON UPDATE CASCADE;
